59961287abdf429a21318e0034f8e55b9d39eee7
snorkelingcode <daneknudsen3d@gmail.com>
2025-02-15 14:26:25 -0600

    test revert

--- js/moduleHandler.js
+++ js/moduleHandler.js
@@ -42,28 +42,21 @@ class ModuleHandler {
         }
     }
 
-async initializeTheme() {
-    try {
-        const walletAddress = SessionManager.getWalletAddress();
-        if (walletAddress) {
-            try {
+    async initializeTheme() {
+        try {
+            const walletAddress = SessionManager.getWalletAddress();
+            if (walletAddress) {
                 const response = await makeApiCall(`${API_ENDPOINTS.users}/profile/${walletAddress}`);
                 if (response && response.theme) {
                     this.currentTheme = response.theme;
                     localStorage.setItem('theme', response.theme);
                     this.applyTheme(response.theme);
                 }
-            } catch (error) {
-                // If user doesn't exist, just use default theme
-                console.log('User profile not found, using default theme');
-                this.applyTheme(this.currentTheme);
             }
+        } catch (error) {
+            console.error('Error loading user theme:', error);
         }
-    } catch (error) {
-        console.log('Using default theme');
-        this.applyTheme(this.currentTheme);
     }
-}
 
     setupEventListeners() {
         if (!this.addButton || !this.modal) return;
